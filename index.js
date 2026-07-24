5d3792519584e5313d3d87cc5773aa23366748e1
Kanishk-1009 <kanishkgupta511@gmail.com>
2026-07-24 10:27:17 +0530

    Updated index and JS function

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 function sum(a, b) {
     return a + b;
 }
-const result = sum(5, 10);
-console.log(`The sum of 5 and 10 is: ${result}`);
+
+function sumWithMsg(clbk,msg) {
+    const result = clbk(5, 10);
+    console.log(msg + ": " + result);
+}
+
+sumWithMsg(sum, "The sum is");
+
+function login(success, error) {
+    if (success) {
+        console.log("Login successful");
+    } else {
+        console.log("Login failed: " + error);
+    }
+}
+function loginVerify(username, password, clbk) {
+    if (username == "admin" && password == "1234") {
+        clbk("success", null);
+    } else {
+        clbk(null,"Invalid username or password");
+    }
+}
+loginVerify("admin", "1234",login)
